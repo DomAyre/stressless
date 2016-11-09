8aecd9617900f3740f64758792cf4a9b9e587012
Dominic Ayre <domayre@outlook.com>
2016-11-09 12:04:26 +0000

    Set up API

---
+++ stressless.py
@@ -0,0 +1,41 @@
+class stressLess():
+    
+    # INPUT
+
+    def getSensorReading(self, frequency=-1):
+        pass
+
+    def getAverageReading(self, time, frequency=-1):
+        pass
+
+    def getMoveDirection(self, frequency=-1):
+        pass
+
+    def getFrequency(self, time, frequency=-1):
+        pass
+
+    def getGripStrength(self, frequency=-1):
+        pass
+
+    def getPalmConfig(self, frequency=-1):
+        pass
+
+    def getStressLevel(self, frequency=-1):
+        pass
+
+    def learn(self, stressLevel, sensorReading):
+        pass
+
+    # OUTPUT
+
+    def setLED(self, red, green, blue, alpha):
+        pass
+
+    def turnOn(self, ):
+        pass
+
+    def turnOff(self, ):
+        pass
+
+    def setSensorFrequency(self, ):
+        pass
---
+++ stressmodel.py
@@ -0,0 +1,2 @@
+class stressModel():
+    pass
