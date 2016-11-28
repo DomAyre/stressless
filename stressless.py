4dc0002d7524ce66a297b6f18f66cd1ed34dc96b
lukaszdygon <lukasz@Lukaszs-MacBook-Pro.local>
2016-11-28 10:21:34 +0000

    started on basic functionality. Added few functions, developed models

--- stressless.py
+++ stressless.py
@@ -1,41 +1,80 @@
-class stressLess():
-    
-    # INPUT
+import time
+import multiprocessing as mp
+from supportFunctions import *
+from stressmodel import StressModel, Reading
 
-    def getSensorReading(self, frequency=-1):
-        pass
 
-    def getAverageReading(self, time, frequency=-1):
-        pass
+class StressLess():
+    def __init__(self):
+        self.device = StressModel()
+        self.activeProcesses = []
 
-    def getMoveDirection(self, frequency=-1):
-        pass
+    def getSensorReading(self):
+        return self.device.getReadings(1)[0]
 
-    def getFrequency(self, time, frequency=-1):
-        pass
+    def getAverageReading(self, number_of_readings=0):
+
+        if number_of_readings == 0:
+            avgReading = self.device.getSessionAvgReadings()
+        else:
+            readings = self.device.getReadings(number_of_readings)
+            avg = [0 for _ in range(len(readings[0].pressure) + 1)]
+            for reading in readings:
+                avg[0] += reading.time
+                for j, el2 in enumerate(reading.pressure):
+                    avg[j + 1] += el2.pressure
+            avg = [a/len(readings) for a in avg]
+            avgReading = Reading(avg)
+        return avgReading
+
+    def getFrequency(self, number_of_readings=25):
+        readings = self.device.getReadings(number_of_readings)
+        time = readings[0].Time() - readings[-1].Time()
+        threshold = self.device.getPressureThreshold()
+
+        number_of_squeezes = 0
+        activated = False
+        for reading in readings:
+            if not activated and max(reading.pressure) > threshold:
+                activated = True
+                number_of_squeezes += 1
+            if activated and max(reading.pressure) < threshold:
+                activated = False
+        return number_of_squeezes / time
 
     def getGripStrength(self, frequency=-1):
         pass
 
     def getPalmConfig(self, frequency=-1):
-        pass
+        reading = self.device.getReadings(1)[0]
+        threshold = self.device.getReadingFrequency()
+        configuration = [p > threshold for p in reading.pressure]
+        return configuration
 
     def getStressLevel(self, frequency=-1):
+        # get
         pass
 
-    def learn(self, stressLevel, sensorReading):
+    def learn(self, stressLevel, sensorReading=False):
+        if not sensorReading:
+            sensorReading = self.device.getReadings(1)
         pass
+
+    def adjust_threshold(self, squeeze_pressure_readings, non_squeeze_pressure_readings):
+        avg_squeeze_reading = sum(sum(r) for r in squeeze_pressure_readings)
+        avg_non_squeeze_reading = sum(sum(r) for r in non_squeeze_pressure_readings)
+        self.device.setPressureThreshold((avg_squeeze_reading + avg_non_squeeze_reading) / 2)
 
     # OUTPUT
 
     def setLED(self, red, green, blue, alpha):
         pass
 
-    def turnOn(self, ):
+    def turnOn(self):
         pass
 
-    def turnOff(self, ):
+    def turnOff(self):
         pass
 
-    def setSensorFrequency(self, ):
-        pass
+    def setSensorFrequency(self, frequency):
+        self.device.setReadingFrequency()
--- stressmodel.py
+++ stressmodel.py
@@ -1,2 +1,36 @@
-class stressModel():
-    pass
+class StressModel():
+    def __init__(self):
+        self.data = []  # list of reading objects
+        self.average = []
+        self.frequency = 1
+        self.threshold = 0
+
+    def readData(self):
+        data = [0, [0,30,10]]  # placeholder for the future. Should read data from device
+        reading = Reading(data)
+
+    def set_average(self):
+        self.average = sum(self.data)/len(self.data)
+
+    def getReadings(self, number_of_readings):
+        if number_of_readings > len(self.data):
+            number_of_readings = len(self.data)
+        return self.data[number_of_readings:]
+
+    def getReadingFrequency(self):
+        return self.frequency
+
+    def setReadingFrequency(self, frequency):
+        self.frequency = frequency
+
+    def setPressureThreshold(self, threshold):
+        self.threshold = threshold
+
+    def getPressureThreshold(self):
+        return self.threshold
+
+
+class Reading():
+    def __init__(self, data):
+        self.time = data[0]
+        self.pressure = data[1]
